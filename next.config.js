838fa045c2478b0ae261ef5283b1d697e9c0b8a0
charyung <charmaineyung@yahoo.com.hk>
2021-07-28 21:06:52 -0400

    hopefully fix deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
     reactStrictMode: true,
     env: {
         CONTENTFUL_API_KEY: process.env.CONTENTFUL_API_KEY,
         CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID
-    }
+    },
+    assetPrefix: isProd ? "/gallery" : ""
 };
